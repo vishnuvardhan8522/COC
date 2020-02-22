04ae33cd299161e9391b392c02388c186aa2a72b
1894987 <vishnuvardhan8522@gmail.com>
2020-02-22 14:07:05 -0500

    initial commit

---
+++ app/src/main/java/com/example/coc/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.coc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+    }
+
+    public void countUp(View view) {
+    }
+}
